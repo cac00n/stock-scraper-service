41875ed1681114c388b101f8bde8123921b18315
k4tzi <kikiwi77@gmail.com>
2023-08-04 21:40:16 +0200

    feat: generate url

---
+++ stock_scraper_service/url_info.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+
+class Currency(str, Enum):
+    eur: str = "EUR"
+    usd: str = "USD"
+
+
+class StockMarket(str, Enum):
+    xetra: str = "3"
+
+
+class BaseUrl(str, Enum):
+    ariva: str = 'https://www.ariva.de/IE00B4L5Y983/kurse/historische-kurse?go=1&boerse_id={stock_market}&month=&currency={currency}&clean_split=1&clean_payout=1&clean_bezug=1'
+
+
+
